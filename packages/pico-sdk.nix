4c516eec35fcd9e69fac9f63be2e6552718e2a4f
paperdev <51487819+paperdev-code@users.noreply.github.com>
2023-03-24 12:09:05 +0100

    attempt 2

--- packages/pico-sdk.nix
+++ packages/pico-sdk.nix
@@ -11,12 +11,7 @@ stdenv.mkDerivation rec {
     hash = "sha256-p69go8KXQR21szPb+R1xuonyFj+ZJDunNeoU7M3zIsE=";
   };
 
-  btstack = fetchFromGitHub {
-    owner = "bluekitchen";
-    repo = "btstack";
-    rev = "0d212321a995ed2f9a80988f73ede854c7ad23b8";
-    hash = "sha256-EkeHDBwL5+BzBQl2j9IMJdDMSgdIkQTBOpJrE5W/Fuo=";
-  };
+  btstack = import ./submodules/btstack.nix { inherit fetchFromGitHub; };
 
   cyw43-driver = fetchFromGitHub {
     owner = "georgerobotics";

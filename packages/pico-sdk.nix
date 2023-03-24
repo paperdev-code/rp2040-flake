7aace8a1d51713db2536fe409072c34ce975b1e2
paperdev <51487819+paperdev-code@users.noreply.github.com>
2023-03-24 12:26:59 +0100

    remodule'd

--- packages/pico-sdk.nix
+++ packages/pico-sdk.nix
@@ -11,17 +11,7 @@ stdenv.mkDerivation rec {
     hash = "sha256-p69go8KXQR21szPb+R1xuonyFj+ZJDunNeoU7M3zIsE=";
   };
 
-  btstack = import ./submodules/btstack.nix { inherit fetchFromGitHub; };
-
-  cyw43-driver = import ./submodules/cyw43-driver.nix { inherit fetchFromGitHub; };
-
-  lwip = import ./submodules/lwip.nix { inherit fetchFromGitHub; };
-
-  mbedtls = import ./submodules/mbedtls.nix { inherit fetchFromGitHub; };
-
-  tinyusb = import ./submodules/tinyusb.nix { inherit fetchFromGitHub; };
-
-  # submodules = import ./submodules { inherit fetchFromGitHub callPackage; };
+  submodules = import ./submodules { inherit fetchFromGitHub; };
 
   nativeBuildInputs = [ cmake ];
 
@@ -31,11 +21,11 @@ stdenv.mkDerivation rec {
     SDK_DIR=$out/lib/pico-sdk
     mkdir -p $SDK_DIR
     mkdir -p $SDK_DIR/lib
-    cp -R ${btstack}/. $SDK_DIR/lib/btstack
-    cp -R ${cyw43-driver}/. $SDK_DIR/lib/cyw43-driver
-    cp -R ${lwip}/. $SDK_DIR/lib/lwip
-    cp -R ${mbedtls}/. $SDK_DIR/lib/mbedtls
-    cp -R ${tinyusb}/. $SDK_DIR/lib/tinyusb
+    cp -R ${submodules.btstack}/. $SDK_DIR/lib/btstack
+    cp -R ${submodules.cyw43-driver}/. $SDK_DIR/lib/cyw43-driver
+    cp -R ${submodules.lwip}/. $SDK_DIR/lib/lwip
+    cp -R ${submodules.mbedtls}/. $SDK_DIR/lib/mbedtls
+    cp -R ${submodules.tinyusb}/. $SDK_DIR/lib/tinyusb
     cp -a ../../../* $SDK_DIR
     chmod +x $SDK_DIR/tools/pioasm/build/pioasm
   '';

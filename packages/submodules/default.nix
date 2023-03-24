c2b0dfd48c14e18cbe245af0ebbb3c9a8a072bc5
paperdev <51487819+paperdev-code@users.noreply.github.com>
2023-03-24 12:27:48 +0100

    untypo'd

--- packages/submodules/default.nix
+++ packages/submodules/default.nix
@@ -1,7 +1,7 @@
 { fetchFromGitHub }: {
-  btstack = import ./submodules/btstack.nix { inherit fetchFromGitHub; };
-  cyw43-driver = import ./submodules/cyw43-driver.nix { inherit fetchFromGitHub; };
-  lwip = import ./submodules/lwip.nix { inherit fetchFromGitHub; };
-  mbedtls = import ./submodules/mbedtls.nix { inherit fetchFromGitHub; };
-  tinyusb = import ./submodules/tinyusb.nix { inherit fetchFromGitHub; };
+  btstack = import ./btstack.nix { inherit fetchFromGitHub; };
+  cyw43-driver = import ./cyw43-driver.nix { inherit fetchFromGitHub; };
+  lwip = import ./lwip.nix { inherit fetchFromGitHub; };
+  mbedtls = import ./mbedtls.nix { inherit fetchFromGitHub; };
+  tinyusb = import ./tinyusb.nix { inherit fetchFromGitHub; };
 }
